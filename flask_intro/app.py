f6de714d8d7195a36efe8acbf73e66a0df309864
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-01 15:45:30 +0200

    initial commit

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -3,3 +3,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "my_secret_key"
+
+todos = ['flask_setup', 'write_code', 'run_code']
+@app.route("/")
+def index():
+    return render_template("index.html", todos = todos)
+
+# dynamic routes
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
