7b47e79be78bd708889c71a2a039e109aedd87c1
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-01 21:44:53 +0200

    SQLALchemy local setup

--- task_manager/app.py
+++ task_manager/app.py
@@ -3,13 +3,23 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # we dont want to track modifications in database
 
-# for practice lets save data locally
-tasks = ['vscode_setup', 'flask_setup', 'happy_coding']
+db = SQLAlchemy(app) # it will connect the databse with app
+
+# model defines how datastructure look likes
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    task_text = db.Column(db.String(150), index =True) 
+
+
+tasks = []
 
 # form to ask user for input
 
@@ -17,10 +27,15 @@ class taskForm(FlaskForm):
     task = StringField("Task") #label
     submit = SubmitField("Add Task")
 
+# how to initialize the database
+db.create_all()       
+
 @app.route("/", methods = ["GET", "POST"])
 def index():
     # some logic so that our router handle the post request
     if 'task' in request.form:
-        tasks.append(request.form['task'])
+        #tasks.append(request.form['task'])
+        db.session.add(Task(task_text = request.form['task'])) # now we are adding in database
+        db.session.commit()
 
-    return render_template("index.html", tasks = tasks, template_form = taskForm()) # we need pass newinstance object of class form
+    return render_template("index.html", tasks = Task.query.all(), template_form = taskForm()) # we need pass newinstance object of class form
---
+++ task_manager/task.py
@@ -0,0 +1,12 @@
+# importing database and model from app.py
+from app import db, Task 
+
+# first_task = Task(task_text = 'lets play with flask')
+# db.session.add(first_task) # how to store in database
+# db.session.commit()
+
+# lets run task.py it will add task in databse
+
+# how to access the data in database
+all_tasks = Task.query.all()
+print(all_tasks)
