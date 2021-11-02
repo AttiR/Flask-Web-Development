473e33aa325f644fa891edba7731f4d676c76206
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-02 21:53:40 +0200

    forms validators

--- flask_forms/app.py
+++ flask_forms/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from flask_wtf.recaptcha import validators
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
 from flask_bootstrap import Bootstrap
 from dotenv import load_dotenv
 load_dotenv()
@@ -12,8 +14,11 @@ app.config['SECRET_KEY'] = os.getenv("MY_KEY")
 bootstrap = Bootstrap(app)
 
 class Myform(FlaskForm):
-    text = StringField('Name')
+    text = StringField('Name', validators = [DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators= [DataRequired(), Length(min=8, max=15)])
     submit = SubmitField('Submit')
+
 
     
 
