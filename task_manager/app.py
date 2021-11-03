5be4775bd31963c18c072a067eaa4763b6de6a80
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-03 22:35:37 +0200

    modified version

--- task_manager/app.py
+++ task_manager/app.py
@@ -1,14 +1,20 @@
 # A simple to do app to cick off with databse and forms in Flask
 
+from re import template
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = os.getenv('MY_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # we dont want to track modifications in database
 
@@ -34,10 +40,11 @@ db.create_all()
 @app.route("/", methods = ["GET", "POST"])
 def index():
     # some logic so that our router handle the post request
-   
-    if 'task' in request.form:
-        #tasks.append(request.form['task'])
-        db.session.add(Task(task_text = request.form['task'])) # now we are adding in database
-        db.session.commit()
+    template_form = taskForm()
+    if template_form.validate_on_submit():
+        if 'task' in request.form:
+            #tasks.append(request.form['task'])
+            db.session.add(Task(task_text = request.form['task'])) # now we are adding in database
+            db.session.commit()
 
-    return render_template("index.html", tasks = Task.query.all(), template_form = taskForm()) # we need pass newinstance object of class form
+        return render_template("index.html", tasks = Task.query.all(), template_form = template_form) # we need pass newinstance object of class form
