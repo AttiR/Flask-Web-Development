7d34d85573cbab85116c64b61688ad3e6925f9de
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-06 15:16:36 +0200

    form-access

--- app/app/views.py
+++ app/app/views.py
@@ -1,6 +1,8 @@
-from flask.templating import render_template
+
+from werkzeug.utils import redirect
 from app import app # so that its run as it is in root
-from flask import render_template
+from flask import render_template, request, url_for, redirect
+
 
 @app.route("/")
 def index():
@@ -30,4 +32,25 @@ def jinja():
     fruits = ['apple', 'bnana', 'mango']
 
     my_html= "<h1>hello</h1>"
-    return render_template("public/jinja.html", name=name, fruits=fruits, age=age, info=info, my_html=my_html)   
+    return render_template("public/jinja.html", name=name, fruits=fruits, age=age, info=info, my_html=my_html)
+    
+@app.route("/sign-up")
+def register():
+    
+    return render_template("public/signup.html")
+
+@app.route("/login")
+def login():
+   
+    return render_template("public/login.html")
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+
+    if request.method == "POST":
+        req= request.form # return as disctionary
+        name = req['name'] # req.get('name) # request.form['name']
+        print(name)
+        return redirect(request.url)
+    
+    return render_template("public/contact.html")        
