5dd9c829ea2872387ebde0948a7ed722f24ff3bd
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-07 18:48:33 +0200

    psotgre Database

--- feedback_form/app.py
+++ feedback_form/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, flash, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 from flask.templating import render_template
 from flask_mail import Mail, Message
 from form import FeedbackForm
@@ -9,11 +9,43 @@ load_dotenv()
 import os
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 mail= Mail(app)
 app.config['SECRET_KEY'] = os.getenv("MY_KEY")
+password = os.getenv('DATABASE_PASS')
+
+ENV = 'dev'
+if ENV == 'dev':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/flask_feedback'
+    app.debug = True
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.debug = False
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# create database object
+db = SQLAlchemy(app)
+
+# Database Model
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    sno = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(200))
+    email= db.Column(db.String(200))
+    feedback = db.Column(db.Text())
+
+    # now we need to define the constructor
+    def __init__(self, name, email, feedback):
+        self.name=name
+        self.email=email
+        self.feedback=feedback
+
+
+
 
     
-bootstrap = Bootstrap(app)
+
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'attirehman388@gmail.com'
@@ -31,6 +63,11 @@ def contact():
         name = req['name']
         email = req['email']
         feedback = req['feedback']
+
+        # databse entry
+        data = Feedback(name, email, feedback)
+        db.session.add(data)
+        db.session.commit()
         
         # how to sent an email
         msg = Message('Feedback notification', sender = 'attirehman388@gmail.com', recipients = [email])
