3668f7f537f7b0b505aa98a1da20b10092a9e628
AttiR <77799634+AttiR@users.noreply.github.com>
2021-11-09 22:34:53 +0200

    app.config and json data

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, config
 
 app = Flask(__name__)
+
+if app.config['ENV'] == 'production':
+    app.config.from_object('config.ProductionConfig')
+
+elif app.config['ENV'] == 'development':
+    app.config.from_object('config.DevelopmentConfig') 
+else:
+     app.config.from_object('config.TestingConfig')
+
+
 
 from app import views # import views from app package
 from app import admin_views
